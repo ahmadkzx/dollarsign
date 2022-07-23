5b45731dfb04d352ad137da95a59644931981771
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-07-23 12:15:27 +0430

    feat: pascal case to snake case utility

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as $separateNumber } from './number/separateNumber'
 
 // string
 export { default as $camelCaseToSnakeCase } from './string/camelCaseToSnakeCase'
+export { default as $pascalCaseToSnakeCase } from './string/pascalCaseToSnakeCase'
---
+++ src/string/pascalCaseToSnakeCase.ts
@@ -0,0 +1,12 @@
+/**
+ * convert pascal case string to snake case
+ * @param {string} payload
+ * @return {string}
+ */
+export default function $pascalCaseToSnakeCase(payload: string) {
+  if (typeof payload != 'string') throw new Error('$camelCaseToSnakeCase: payload is not string')
+
+  return payload
+    .replace(/(?:^|\.?)([A-Z])/g, (_, letter) => '_' + letter.toLowerCase())
+    .replace(/^_/, '')
+}
