76ac92357d879a9c272e22147cd3e4da90251022
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-07-26 11:26:16 +0430

    fix: add kebab case to camel case utility in index file

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { default as $separateNumber } from './number/separateNumber'
 // string
 export { default as $camelCaseToSnakeCase } from './string/camelCaseToSnakeCase'
 export { default as $kebabCaseToSnakeCase } from './string/kebabCaseToSnakeCase'
+export { default as $kebabCaseToCamelCase } from './string/kebabCaseToCamelCase'
 export { default as $pascalCaseToSnakeCase } from './string/pascalCaseToSnakeCase'
